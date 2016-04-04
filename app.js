fc8f513ee0a83681718e996a9ef4293d7d5b36e6
Chris Cooper <chris.cooper@nicta.com.au>
2016-04-04 11:22:51 +1000

    this might even be working

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-require('cesium/Source/Widgets/widgets.css');
-var BuildModuleUrl = require('cesium/Source/Core/buildModuleUrl');
+require('terriajs-cesium/Source/Widgets/widgets.css');
+var BuildModuleUrl = require('terriajs-cesium/Source/Core/buildModuleUrl');
 BuildModuleUrl.setBaseUrl('./');
 
-var Viewer = require('cesium/Source/Widgets/Viewer/Viewer');
-var CesiumTerrainProvider = require('cesium/Source/Core/CesiumTerrainProvider');
+var Viewer = require('terriajs-cesium/Source/Widgets/Viewer/Viewer');
+var CesiumTerrainProvider = require('terriajs-cesium/Source/Core/CesiumTerrainProvider');
+var PropellerTerrainProvider = require('propeller-js/lib/Cesium/CesiumTerrainProvider');
 
 var viewer = new Viewer('cesiumContainer');
 
-var cesiumTerrainProviderMeshes = new CesiumTerrainProvider({
-    url: 'https://assets.agi.com/stk-terrain/world',
-    requestWaterMask: true,
-    requestVertexNormals: true
-});
+document.propeller = {};
 
-viewer.terrainProvider = cesiumTerrainProviderMeshes;
+if (1) {
+
+    var cesiumTerrainProviderMeshes = new CesiumTerrainProvider({
+        url: 'https://assets.agi.com/stk-terrain/world',
+        requestWaterMask: true,
+        requestVertexNormals: true
+    });
+
+    viewer.terrainProvider = cesiumTerrainProviderMeshes;
+
+} else {
+
+    var propellerTerrainProvider = new PropellerTerrainProvider({
+        url: 'https://assets.agi.com/stk-terrain/world',
+        requestWaterMask: true,
+        requestVertexNormals: true
+    });
+
+    viewer.terrainProvider = propellerTerrainProvider;
+
+}
---
+++ postinstall.sh
@@ -0,0 +1,6 @@
+rm -rf public && mkdir public
+#cd node_modules/terriajs-cesium
+#npm install
+#npm run release
+#cd ../../
+cp -r ./node_modules/terriajs-cesium/wwwroot/build/* ./public
