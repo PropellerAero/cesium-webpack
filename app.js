473b59a88bbd5f1f903d5b7ba15fbe9f027eaee4
Chris Cooper <chris.cooper@nicta.com.au>
2016-04-04 11:41:03 +1000

    upgrades

--- app.js
+++ app.js
@@ -3,5 +3,14 @@ var BuildModuleUrl = require('cesium/Source/Core/buildModuleUrl');
 BuildModuleUrl.setBaseUrl('./');
 
 var Viewer = require('cesium/Source/Widgets/Viewer/Viewer');
+var CesiumTerrainProvider = require('cesium/Source/Core/CesiumTerrainProvider');
 
 var viewer = new Viewer('cesiumContainer');
+
+var cesiumTerrainProviderMeshes = new CesiumTerrainProvider({
+    url: 'https://assets.agi.com/stk-terrain/world',
+    requestWaterMask: true,
+    requestVertexNormals: true
+});
+
+viewer.terrainProvider = cesiumTerrainProviderMeshes;
