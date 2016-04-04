7d9d8e26b991c755dbec62346b343ad068332b9d
Chris Cooper <chris.cooper@nicta.com.au>
2016-04-04 11:31:07 +1000

    trim back app

--- app.js
+++ app.js
@@ -4,30 +4,13 @@ BuildModuleUrl.setBaseUrl('./');
 
 var Viewer = require('terriajs-cesium/Source/Widgets/Viewer/Viewer');
 var CesiumTerrainProvider = require('terriajs-cesium/Source/Core/CesiumTerrainProvider');
-var PropellerTerrainProvider = require('propeller-js/lib/Cesium/CesiumTerrainProvider');
 
 var viewer = new Viewer('cesiumContainer');
 
-document.propeller = {};
+var cesiumTerrainProviderMeshes = new CesiumTerrainProvider({
+    url: 'https://assets.agi.com/stk-terrain/world',
+    requestWaterMask: true,
+    requestVertexNormals: true
+});
 
-if (1) {
-
-    var cesiumTerrainProviderMeshes = new CesiumTerrainProvider({
-        url: 'https://assets.agi.com/stk-terrain/world',
-        requestWaterMask: true,
-        requestVertexNormals: true
-    });
-
-    viewer.terrainProvider = cesiumTerrainProviderMeshes;
-
-} else {
-
-    var propellerTerrainProvider = new PropellerTerrainProvider({
-        url: 'https://assets.agi.com/stk-terrain/world',
-        requestWaterMask: true,
-        requestVertexNormals: true
-    });
-
-    viewer.terrainProvider = propellerTerrainProvider;
-
-}
+viewer.terrainProvider = cesiumTerrainProviderMeshes;
